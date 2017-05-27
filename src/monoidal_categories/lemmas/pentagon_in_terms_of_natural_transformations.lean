19a01f6cc7b39cf18f3f75d0e868d3c0e4fc96b0
Scott Morrison <scott.morrison@anu.edu.au>
2017-05-27 22:19:01 +1000

    Removing the bad apply again.

--- src/monoidal_categories/lemmas/pentagon_in_terms_of_natural_transformations.lean
+++ src/monoidal_categories/lemmas/pentagon_in_terms_of_natural_transformations.lean
@@ -12,17 +12,17 @@ namespace tqft.categories.monoidal_category
 
 universe variables u v
 
-lemma pentagon_in_terms_of_natural_transformations
-   { C : Category.{u v} } ( m : MonoidalStructure C ) :
-  pentagon_3step m = pentagon_2step m :=
-  begin 
-    dsimp,
-    -- FIXME this apply takes 15 minutes ???
-    apply NaturalTransformations_componentwise_equal,
-    {
-      tidy,
-      exact m.pentagon _ _ _ _ -- TODO it would be nice if this could be automated...
-    },
-end
+-- lemma pentagon_in_terms_of_natural_transformations
+--    { C : Category.{u v} } ( m : MonoidalStructure C ) :
+--   pentagon_3step m = pentagon_2step m :=
+--   begin 
+--     dsimp,
+--     -- FIXME this apply takes 15 minutes ???
+--     apply NaturalTransformations_componentwise_equal,
+--     {
+--       tidy,
+--       exact m.pentagon _ _ _ _ -- TODO it would be nice if this could be automated...
+--     },
+-- end
 
 end tqft.categories.monoidal_category
