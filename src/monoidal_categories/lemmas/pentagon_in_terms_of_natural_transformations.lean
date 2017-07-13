f16e105ed7ac78cc318564c5db2432e4bad0dfcc
Scott Morrison <scott.morrison@anu.edu.au>
2017-07-13 18:45:23 -0500

    apply no longer takes 15 minutes

--- src/monoidal_categories/lemmas/pentagon_in_terms_of_natural_transformations.lean
+++ src/monoidal_categories/lemmas/pentagon_in_terms_of_natural_transformations.lean
@@ -2,6 +2,7 @@
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Authors: Stephen Morgan, Scott Morrison
 import .pentagon_in_terms_of_natural_transformations_definitions
+import tidy.its
 
 open categories
 open categories.functor
@@ -12,17 +13,21 @@ namespace categories.monoidal_category
 
 universe variables u v
 
--- lemma pentagon_in_terms_of_natural_transformations
---    { C : Category.{u v} } ( m : MonoidalStructure C ) :
---   pentagon_3step m = pentagon_2step m :=
---   begin 
---     dsimp,
---     -- FIXME this apply takes 15 minutes ???
---     apply NaturalTransformations_componentwise_equal,
---     {
---       tidy,
---       exact m.pentagon _ _ _ _ -- TODO it would be nice if this could be automated...
---     },
--- end
+lemma pentagon_in_terms_of_natural_transformations
+   { C : Category.{u v} } ( m : MonoidalStructure C ) :
+  pentagon_3step m = pentagon_2step m :=
+  begin 
+    dsimp',
+    apply NaturalTransformations_componentwise_equal,
+    intros WXYZ,
+    induction WXYZ with WXY Z,
+    induction WXY with WX Y,
+    induction WX with W X,
+    {
+      tidy,
+      have p := m.pentagon W X Y Z,
+      tidy,
+    },
+end
 
 end categories.monoidal_category
