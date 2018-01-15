9b252b79fdaaec0177a1c85e34ad690d415074f9
Scott Morrison <scott@tqft.net>
2018-01-15 13:56:34 +1100

    testing

---
+++ src/monoidal_categories/monoidal_structure_from_products.1.lean
@@ -0,0 +1,21 @@
+-- Copyright (c) 2017 Scott Morrison. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Authors: Scott Morrison
+import .monoidal_structure_from_products
+import categories.types
+import categories.universal.types
+
+open categories
+open categories.functor
+open categories.products
+open categories.natural_transformation
+open categories.monoidal_category
+open categories.universal
+open categories.types
+
+namespace categories.monoidal_category
+
+#eval Symmetry_on_MonoidalStructure_from_Products CategoryOfTypes
+
+
+end categories.monoidal_category
