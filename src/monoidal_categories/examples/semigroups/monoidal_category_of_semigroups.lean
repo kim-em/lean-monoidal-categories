6c59a3275fc557282af42d3f6c4232810bd044cd
Scott Morrison <scott@tqft.net>
2018-01-30 09:15:58 +1100

    constructing the symmetric monoidal category of semigroups, using the fact that products exist

--- src/monoidal_categories/examples/semigroups/monoidal_category_of_semigroups.lean
+++ src/monoidal_categories/examples/semigroups/monoidal_category_of_semigroups.lean
@@ -12,116 +12,11 @@ open categories.monoidal_category
 
 namespace categories.examples.semigroups
 
--- PROJECT really this should be a special case of the (uniquely braided, symmetric) monoidal structure coming from a product.
-
 open categories.products
 open categories.universal
 definition MonoidalStructureOnCategoryOfSemigroups : MonoidalStructure CategoryOfSemigroups := MonoidalStructure_from_Products CategoryOfSemigroups
 
--- Below, I was doing it directly:
-
--- definition TensorProduct_for_Semigroups : TensorProduct CategoryOfSemigroups := {
---     onObjects     := λ p, ⟨ p.1.1 × p.2.1, semigroup_product p.1.2 p.2.2 ⟩,
---     onMorphisms   := λ s t f, semigroup_morphism_product f.1 f.2,
---     identities    := ♯,
---     functoriality := ♮
---   }
-
--- definition Associator_for_Semigroups : Associator TensorProduct_for_Semigroups := {
---     morphism := {
---       components := λ _, {
---         map := λ t, (t.1.1, (t.1.2, t.2)),
---         multiplicative := ♮
---       },
---       naturality := ♮ 
---     },
---     inverse := {
---       components := λ _, {
---         map := λ t, ((t.1, t.2.1), t.2.2),
---         multiplicative := ♮
---       },
---       naturality := ♮  
---     },
---     witness_1 := ♯,
---     witness_2 := ♯
---   }
-
--- definition TensorUnit_for_Semigroups : CategoryOfSemigroups.Obj := ⟨ punit, trivial_semigroup ⟩  -- punit is just a universe-parameterized version of unit
-
--- definition LeftUnitor_for_Semigroups : @LeftUnitor CategoryOfSemigroups TensorUnit_for_Semigroups TensorProduct_for_Semigroups := {
---     morphism := {
---       components := λ _, {
---         map := λ t, t.2,
---         multiplicative := ♮
---       },
---       naturality := ♮ 
---     },
---     inverse := {
---       components := λ _, {
---         map := λ t, (punit.star, t),
---         multiplicative := ♮
---       },
---       naturality := ♮ 
---     },
---     witness_1 := ♯,
---     witness_2 := ♮
---   }
-
--- definition RightUnitor_for_Semigroups : @RightUnitor CategoryOfSemigroups TensorUnit_for_Semigroups TensorProduct_for_Semigroups := {
---     morphism := {
---       components := λ _, {
---         map := λ t, t.1,
---         multiplicative := ♮
---       },
---       naturality := ♮ 
---     },
---     inverse := {
---       components := λ _, {
---         map := λ t, (t, punit.star),
---         multiplicative := ♮
---       },
---       naturality := ♮ 
---     },
---     witness_1 := ♯,
---     witness_2 := ♮
---   }
-
--- definition MonoidalStructureOnCategoryOfSemigroups : MonoidalStructure CategoryOfSemigroups := 
--- {
---   tensor                      := TensorProduct_for_Semigroups,
---   tensor_unit                 := TensorUnit_for_Semigroups,
---   associator_transformation   := Associator_for_Semigroups,
---   left_unitor_transformation  := LeftUnitor_for_Semigroups,
---   right_unitor_transformation := RightUnitor_for_Semigroups,
---   pentagon                    := ♯,
---   triangle                    := ♯
--- }
-
--- open categories.natural_transformation
--- open categories.braided_monoidal_category
-
--- definition SymmetryOnCategoryOfSemigroups : Symmetry MonoidalStructureOnCategoryOfSemigroups := {
---   braiding := {
---     morphism  := {
---       components := λ _, {
---                            map := λ p, (p.2, p.1),
---                            multiplicative := ♮
---                          },
---       naturality := ♮
---     },
---     inverse   := {
---       components := λ _, {
---                            map := λ p, (p.2, p.1), -- PROJECT this is sufficiently obvious that automation should be doing it for us!
---                            multiplicative := ♮
---                          },
---       naturality := ♮
---     },
---     witness_1 := ♯,
---     witness_2 := ♯
---   },
---   hexagon_1 := ♯,
---   hexagon_2 := ♯,
---   symmetry  := ♯
--- }
+open categories.braided_monoidal_category
+definition SymmetryOnCategoryOfSemigroups : Symmetry MonoidalStructureOnCategoryOfSemigroups := Symmetry_on_MonoidalStructure_from_Products CategoryOfSemigroups 
 
 end categories.examples.semigroups
